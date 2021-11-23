f6a1b18bc83260b39bc9d86f8859d0575ad354d3
亮 <oocco@126.com>
2021-11-23 23:02:11 +0800

    first version

---
+++ ucos/os_cfg.h
@@ -0,0 +1,6 @@
+#ifndef OS_CFG_H
+#define OS_CFG_H
+
+#define OS_CFG_PRIO_MAX     32u
+
+#endif
---
+++ ucos/os_type.h
@@ -0,0 +1,10 @@
+#ifndef OS_TYPE_H
+#define OS_TYPE_H
+
+#include "cpu.h"
+
+typedef CPU_INT16U          OS_OBJ_QTY;
+typedef CPU_INT8U           OS_PRIO;
+typedef CPU_INT8U           OS_STATE;
+
+#endif
